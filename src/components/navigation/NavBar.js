bb681ff5f99e201b778156804b7e6bcb757bbb12
Zachary Hardin <hardinzw@gmail.com>
2019-11-21 13:38:18 -0500

    push to make major file arrangements

--- src/components/navigation/NavBar.js
+++ src/components/navigation/NavBar.js
@@ -1,29 +1,41 @@
 import React from 'react';
 import clsx from 'clsx';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Grow from '@material-ui/core/Grow';
+import Paper from '@material-ui/core/Paper';
+import Popper from '@material-ui/core/Popper';
 import Drawer from '@material-ui/core/Drawer';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import List from '@material-ui/core/List';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
+import Grid from '@material-ui/core/Grid';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
+import BugReportTwoToneIcon from '@material-ui/icons/BugReportTwoTone';
+import DashboardTwoToneIcon from '@material-ui/icons/DashboardTwoTone';
 import './style.css'
 
 const drawerWidth = 220;
 
 const useStyles = makeStyles(theme => ({
   root: {
-    display: 'flex',
+    width: '100%'
   },
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
@@ -40,9 +52,10 @@ const useStyles = makeStyles(theme => ({
       duration: theme.transitions.duration.enteringScreen,
     }),
   },
-
+  avatar: {
+  },
   menuButton: {
-    marginRight: 36,
+    marginRight: 20,
   },
   hide: {
     display: 'none',
@@ -70,6 +83,9 @@ const useStyles = makeStyles(theme => ({
       width: theme.spacing(9) + 1,
     },
   },
+  paper: {
+    marginRight: theme.spacing(2),
+  },
   toolbar: {
     display: 'flex',
     alignItems: 'center',
@@ -83,10 +99,45 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-export default function MiniDrawer() {
+export default function Navigation() {
   const classes = useStyles();
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
+  const [selectedIndex, setSelectedIndex] = React.useState(1);
+  const anchorRef = React.useRef(null);
+
+  const handleToggle = () => {
+    setOpen(prevOpen => !prevOpen);
+  };
+
+  const handleClose = event => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  function handleListKeyDown(event) {
+    if (event.key === 'Tab') {
+      event.preventDefault();
+      setOpen(false);
+    }
+  };
+
+  // return focus to the button when we transitioned from !open -> open
+  const prevOpen = React.useRef(open);
+  React.useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
+
+  const handleListItemClick = (event, index) => {
+    setSelectedIndex(index);
+  };
 
   const handleDrawerOpen = () => {
     setOpen(true);
@@ -96,6 +147,7 @@ export default function MiniDrawer() {
     setOpen(false);
   };
 
+  {/*Begin navigation header and sidebar*/}
   return (
     <div className={classes.root}>
       <CssBaseline />
@@ -105,6 +157,8 @@ export default function MiniDrawer() {
           [classes.appBarShift]: open,
         })}
       >
+
+        {/*Navbar Header*/}
         <Toolbar>
           <IconButton
             color="inherit"
@@ -117,11 +171,41 @@ export default function MiniDrawer() {
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" noWrap className="title">
-            Illuminate
-          </Typography>
+          <Grid container justify="space-between" alignItems="center">
+            <Typography variant="h6" noWrap className="title">
+              Illuminate
+            </Typography>
+            <Button           
+              ref={anchorRef}
+              aria-controls={open ? 'menu-list-grow' : undefined}
+              aria-haspopup="menu"
+              onClick={handleToggle}
+            >
+              <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" className={classes.avatar} />
+            </Button>
+            <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal>
+          {({ TransitionProps, placement }) => (
+            <Grow
+              {...TransitionProps}
+              style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
+            >
+              <Paper>
+                <ClickAwayListener onClickAway={handleClose}>
+                  <MenuList autoFocusItem={open} id="menu-list-grow" onKeyDown={handleListKeyDown}>
+                    <MenuItem onClick={handleClose}>Profile</MenuItem>
+                    <MenuItem onClick={handleClose}>My account</MenuItem>
+                    <MenuItem onClick={handleClose}>Logout</MenuItem>
+                  </MenuList>
+                </ClickAwayListener>
+              </Paper>
+            </Grow>
+          )}
+        </Popper>
+          </Grid>
         </Toolbar>
       </AppBar>
+
+      {/*Navigation Sidebar*/}
       <Drawer
         variant="permanent"
         className={clsx(classes.drawer, {
@@ -141,24 +225,40 @@ export default function MiniDrawer() {
             {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
           </IconButton>
         </div>
-        <Divider />
-        <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
+        <div className={classes.root}>
+          <List component="nav" aria-label="main-sidebar">
+            <ListItem
+              button
+              selected={selectedIndex === 0}
+              onClick={event => handleListItemClick(event, 0)}
+            >
+              <ListItemIcon>
+                <DashboardTwoToneIcon />
+              </ListItemIcon>
+              <ListItemText primary="Dashboard" />
             </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
+            <ListItem
+              button
+              selected={selectedIndex === 0}
+              onClick={event => handleListItemClick(event, 0)}
+            >
+              <ListItemIcon>
+                <BugReportTwoToneIcon />
+              </ListItemIcon>
+              <ListItemText primary="Issues" />
             </ListItem>
-          ))}
-        </List>
+            <ListItem
+              button
+              selected={selectedIndex === 0}
+              onClick={event => handleListItemClick(event, 0)}
+            >
+              <ListItemIcon>
+                <BugReportTwoToneIcon />
+              </ListItemIcon>
+              <ListItemText primary="Issues" />
+            </ListItem>
+          </List>
+        </div>
       </Drawer>
       <main className={classes.content}>
         <div className={classes.toolbar} />
